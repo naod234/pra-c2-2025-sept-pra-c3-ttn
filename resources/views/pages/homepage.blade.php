05e140a61a13c7aba1d691c7ab9d7036a61f60aa
naod234 <D295700@edu.curio.nl>
2025-09-25 10:58:08 +0200

    tickt 6
    1

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -1,13 +1,19 @@
 <x-layouts.app>
 
     <x-slot:introduction_text>
-        <p><img src="img/afbl_logo.png" align="right" width="100" height="100">{{ __('introduction_texts.homepage_line_1') }}</p>
-        <p>{{ __('introduction_texts.homepage_line_2') }}</p>
-        <p>{{ __('introduction_texts.homepage_line_3') }}</p>
+        <div class="introduction-text">
+            <p><img src="img/afbl_logo.png" align="right" width="100" height="100">{{ __('introduction_texts.homepage_line_1') }}</p>
+            <p>{{ __('introduction_texts.homepage_line_2') }}</p>
+            <p>{{ __('introduction_texts.homepage_line_3') }}</p>
+        </div>
     </x-slot:introduction_text>
 
-    <div class="brand-list-container">
-        <h1>{{ __('misc.all_brands') }}</h1>
+    <div class="brands-container">
+        <h1>
+            <x-slot:title>
+                {{ __('misc.all_brands') }}
+            </x-slot:title>
+        </h1>
 
         <?php
         $size = count($brands);
@@ -16,36 +22,33 @@
         ?>
 
         <div class="row">
-
             @foreach($brands->chunk($chunk_size) as $chunk)
                 <div class="col-md-4">
+                    <div class="brand-column">
+                        <ul>
+                            @foreach($chunk as $brand)
+                                <?php
+                                $current_first_letter = strtoupper(substr($brand->name, 0, 1));
 
-                    <ul>
-                        @foreach($chunk as $brand)
+                                if (!isset($header_first_letter) || (isset($header_first_letter) && $current_first_letter != $header_first_letter)) {
+                                    echo '</ul>
+                                    <h2>' . $current_first_letter . '</h2>
+                                    <ul>';
+                                }
+                                $header_first_letter = $current_first_letter
+                                ?>
 
-                            <?php
-                            $current_first_letter = strtoupper(substr($brand->name, 0, 1));
-
-                            if (!isset($header_first_letter) || (isset($header_first_letter) && $current_first_letter != $header_first_letter)) {
-                                echo '</ul>
-						<h2>' . $current_first_letter . '</h2>
-						<ul>';
-                            }
-                            $header_first_letter = $current_first_letter
-                            ?>
-
-                            <li>
-                                <a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/">{{ $brand->name }}</a>
-                            </li>
-                        @endforeach
-                    </ul>
-
+                                <li>
+                                    <a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/">{{ $brand->name }}</a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
                 </div>
                 <?php
                 unset($header_first_letter);
                 ?>
             @endforeach
-
         </div>
     </div>
 
